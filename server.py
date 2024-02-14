b3ce34abcb61ad459dd292a074852999faf95a9a
Student Worker <studentworker@dynamic-oit-vapornet100-10-9-79-236.princeton.edu>
2024-02-13 22:11:19 -0500

    yay

--- server.py
+++ server.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from openai import OpenAI
 
-app = Flask(__name__)
+def init_app():
+    app = Flask(__name__)
+    CORS(app)
+    key_file = open('key.txt', mode='r')
+    key = key_file.readline().strip()
+    client = OpenAI(api_key=key)
 
-@app.route("/query")
-def query():
-    return 'Hi! The backend is working'
+    @app.route("/query", methods=['POST'])
+    def query():
+        try:
+            request_data = request.json
+            query = request_data.get('query')
+
+            print(query)
+
+            return {"answer": "Hi! The backend is working"}, 200
+        
+        except Exception as e:
+            return jsonify({'error': str(e)}), 400
+    
+    return app
 
 if __name__ == '__main__':
+    app = init_app()
+    # print(f"KEY: {key}")
     app.run(debug=True)
